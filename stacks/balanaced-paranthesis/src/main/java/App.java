455db9290b5e21655c79823da28cbd123f898a00
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-02-15 08:19:28 -0500

    Adding program to check for balanced paranthesis.

--- stacks/balanaced-paranthesis/src/main/java/App.java
+++ stacks/balanaced-paranthesis/src/main/java/App.java
@@ -1,9 +1,31 @@
+import java.util.Stack;
+
 public class App {
     public static void main(String[] args) {
+        System.out.println(isValid("(]"));
 
     }
 
     public static boolean isValid(String s){
+        Stack<Character> stack= new Stack<>();
+
+        for(int i=0; i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i)== '{' || s.charAt(i)=='['){
+                stack.push(s.charAt(i));
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                Character ch= stack.pop();
+
+                if((ch=='[' && s.charAt(i)!=']') || (ch=='{' && s.charAt(i)!='}')
+                || (ch=='(' && s.charAt(i)!=')')){
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
 
     }
 }
