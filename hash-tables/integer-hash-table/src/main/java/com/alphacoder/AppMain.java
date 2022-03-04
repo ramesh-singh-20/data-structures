4ec827987924f69c8172671644d9eb359bb3ad2e
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-03-04 08:35:45 -0500

    Implementing hash tables.

--- hash-tables/integer-hash-table/src/main/java/com/alphacoder/AppMain.java
+++ hash-tables/integer-hash-table/src/main/java/com/alphacoder/AppMain.java
@@ -1,4 +1,11 @@
 package com.alphacoder;
 
 public class AppMain {
+    public static void main(String[] args) {
+        int i= 3;
+        int k= 8;
+        double l= (double)i/(double)k;
+
+        System.out.println(l);
+    }
 }
--- hash-tables/integer-hash-table/src/main/java/com/alphacoder/HashTable.java
+++ hash-tables/integer-hash-table/src/main/java/com/alphacoder/HashTable.java
@@ -5,13 +5,14 @@ import java.util.concurrent.ThreadLocalRandom;
 public class HashTable {
     private int maxSize;
     private int currentSize;
-    private double loadFactor;
     private int prime;
     private int a;
     private int b;
+    private Entry[] entryArr;
 
     public HashTable(){
         this.maxSize= 8;
+        entryArr= new Entry[maxSize];
         this.prime= nextPrime(maxSize);
         int a = ThreadLocalRandom.current().nextInt(1, prime + 1);
         int b= ThreadLocalRandom.current().nextInt(1, prime+1);
@@ -21,8 +22,41 @@ public class HashTable {
         return (((a*key)+b)%prime)%maxSize;
     }
 
-    public void insert(int key, int value){
-        loadFactor= (double) currentSize/maxSize;
+    public void insert(int key, String value){
+
+        int temp= hash(key);
+        int i= temp;
+
+        do {
+            if (entryArr[i] == null) {
+                Entry entry = new Entry();
+                entry.value = value;
+                entryArr[i] = entry;
+                currentSize++;
+                return;
+            } else if (entryArr[i].isDeleted) {
+                entryArr[i].value = value;
+                entryArr[i].isDeleted = false;
+                currentSize++;
+                return;
+            } else if (entryArr[i].value.equals(value)) {
+                entryArr[i].value = value;
+                return;
+            } else if (!entryArr[i].value.equals(value)) {
+                i = (i + 1) % maxSize;
+            }
+        }while(i!= temp);
+    }
+
+    private void reSize(){
+        if((double)currentSize/(double) maxSize>0.5){
+            maxSize= 2* maxSize;
+            prime= nextPrime(maxSize);
+            a= ThreadLocalRandom.current().nextInt(1, prime + 1);
+            b= ThreadLocalRandom.current().nextInt(1, prime+1);
+            Entry [] tempArr= new Entry[maxSize];
+
+        }
     }
 
 
