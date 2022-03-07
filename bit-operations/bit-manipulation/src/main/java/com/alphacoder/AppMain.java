4af98444f4bdd20f286e1d035ae593d6869e9ec4
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-03-07 16:16:18 -0500

    Learning bit manipulation.

---
+++ bit-operations/bit-manipulation/src/main/java/com/alphacoder/AppMain.java
@@ -0,0 +1,13 @@
+package com.alphacoder;
+
+public class AppMain {
+    public static void main(String[] args) {
+        BitManipulation bitManipulation= new BitManipulation();
+        int num= 6;
+        System.out.println(bitManipulation.setBit(6, 5));
+
+        System.out.println(bitManipulation.clearBit(6, 2));
+
+        System.out.println(bitManipulation.flipBit(6, 0));
+    }
+}
--- bit-operations/bit-manipulation/src/main/java/com/alphacoder/BitManipulation.java
+++ bit-operations/bit-manipulation/src/main/java/com/alphacoder/BitManipulation.java
@@ -1,4 +1,28 @@
 package com.alphacoder;
 
 public class BitManipulation {
+
+    public int setBit(int number, int position){
+        int mask=1;
+        mask= mask << position;
+        number= number|mask;
+
+        return number;
+    }
+
+    public int clearBit(int number, int position){
+        int mask=1;
+        mask= mask<< position;
+        mask= ~mask;
+        number= number & mask;
+        return number;
+    }
+
+    public int flipBit(int number, int position){
+        int mask=1;
+        mask= mask << position;
+        number= number ^ mask;
+
+        return number;
+    }
 }
