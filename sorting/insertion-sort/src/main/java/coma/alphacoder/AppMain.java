a364adb14ffbb25d383019bf83c6c8293815c5c6
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-03-19 07:48:27 -0400

    Implementing insertion-sort.

---
+++ sorting/insertion-sort/src/main/java/coma/alphacoder/AppMain.java
@@ -0,0 +1,11 @@
+package coma.alphacoder;
+
+import java.util.Arrays;
+
+public class AppMain {
+    public static void main(String[] args) {
+        InsertionSort sort= new InsertionSort();
+
+        System.out.println(Arrays.toString(sort.insertionSort(new int[] {3, 6, 1, 4, 2, 8, 5, 1})));
+    }
+}
