2007c8d1b4af7393f504db27bdf37bfd3cfd1d4d
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-03-19 08:46:46 -0400

    Optimizing bubble sort.

--- sorting/bubble-sort/src/main/java/com/alphacoder/AppMain.java
+++ sorting/bubble-sort/src/main/java/com/alphacoder/AppMain.java
@@ -2,10 +2,13 @@ package com.alphacoder;
 
 import java.util.Arrays;
 
+
 public class AppMain {
     public static void main(String[] args) {
         BubbleSort sort= new BubbleSort();
 
-        System.out.println(Arrays.toString(sort.bubbleSort(new int[]{2, 1})));
+        System.out.println(Arrays.toString(sort.bubbleSort(new int[]{3,4,5,6, 76, 1, 5, 8, 1})));
+
+
     }
 }
--- sorting/bubble-sort/src/main/java/com/alphacoder/BubbleSort.java
+++ sorting/bubble-sort/src/main/java/com/alphacoder/BubbleSort.java
@@ -5,12 +5,17 @@ public class BubbleSort {
     public int[] bubbleSort(int [] nums){
 
         for(int i=nums.length-1; i> 0; i--) {
+            int flag=0;
             for (int j = 0; j <i; j++){
                 if(nums[j]> nums[j+1]){
                     int temp= nums[j];
                     nums[j]= nums[j+1];
                     nums[j+1]= temp;
+                    flag=1;
                 }
+            }
+            if(flag==0){
+                break;
             }
         }
 
