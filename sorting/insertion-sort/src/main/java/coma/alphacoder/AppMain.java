17b8733d0b841c73d45519a682476074441f1d40
Ramesh Singh <singh.ramesh.eee@gmail.com>
2022-03-19 08:04:57 -0400

    Optimizing Insertion sort.

--- sorting/insertion-sort/src/main/java/coma/alphacoder/AppMain.java
+++ sorting/insertion-sort/src/main/java/coma/alphacoder/AppMain.java
@@ -6,6 +6,6 @@ public class AppMain {
     public static void main(String[] args) {
         InsertionSort sort= new InsertionSort();
 
-        System.out.println(Arrays.toString(sort.insertionSort(new int[] {3, 6, 1, 4, 2, 8, 5})));
+        System.out.println(Arrays.toString(sort.insertionSort(new int[] {3, 6, 1, 76, 34, 65, 78})));
     }
 }
--- sorting/insertion-sort/src/main/java/coma/alphacoder/InsertionSort.java
+++ sorting/insertion-sort/src/main/java/coma/alphacoder/InsertionSort.java
@@ -6,7 +6,10 @@ public class InsertionSort {
 
         for(int i=1; i< nums.length; i++){
             for(int j=i; j>0; j--){
-                if(nums[j]< nums[j-1]){
+                if(nums[j]> nums[j-1]){
+                    break;
+                }
+                if(nums[j-1]> nums[j]){
                     int temp= nums[j];
                     nums[j]= nums[j-1];
                     nums[j-1]= temp;
